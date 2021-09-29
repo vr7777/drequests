33505c4fa7b5fae0dcf88d7b220cf3a1ec4dc5d9
vr <77085326+downvert1@users.noreply.github.com>
2021-09-29 15:46:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     author="vr",
     description="Discord requests wrapper",
     url="https://github.com/downvert1/drequests",
+    download_url="https://github.com/downvert1/drequests/archive/refs/heads/main.zip",
     packages=setuptools.find_packages(),
     classifiers=[],
     install_requires=requirements

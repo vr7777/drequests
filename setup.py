f0d0e8b96487652b809f73da08e1a6ccf751b495
vr <77085326+downvert1@users.noreply.github.com>
2021-09-29 15:24:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+import setuptools
 
+with open("requirements.txt") as fp:
+    requirements = fp.read().splitlines()
+
+setuptools.setup(
+    name="drequests",
+    author="vr",
+    description="Discord requests wrapper",
+    url="https://github.com/downvert1/drequests",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    install_requires=requirements
+)
